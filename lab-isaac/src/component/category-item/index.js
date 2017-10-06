ddabba7bc5b7635b36abae5afdf308d14cdd6bff
esack7 <isaacheist@gmail.com>
2017-10-05 17:26:34 -0700

    expenses are now working, still need to style

--- lab-isaac/src/component/category-item/index.js
+++ lab-isaac/src/component/category-item/index.js
@@ -1,51 +1,77 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import CategoryForm from '../category-form';
+import ExpenseItem from '../expense-item';
 import ExpenseForm from '../expense-form';
-import {categoryUpdate, categoryDelete} from '../../action/category-actions';
+import CategoryForm from '../category-form';
 import {expenseCreate} from '../../action/expense-actions';
+import {categoryUpdate, categoryDelete} from '../../action/category-actions';
 
 class CategoryItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categoryEdit: false,
+      expenseForm: false,
+      categoryForm: false,
     };
-    this.toggleUpdate = this.toggleUpdate.bind(this);
+
+    this.toggleExpense = this.toggleExpense.bind(this);
+    this.toggleCategory = this.toggleCategory.bind(this);
   }
 
-  toggleUpdate(e) {
-    this.setState({
-      categoryEdit: !this.state.categoryEdit,
-    });
+  toggleExpense() {
+    this.setState({expenseForm: !this.state.expenseForm});
+  }
+
+  toggleCategory() {
+    this.setState({categoryForm: !this.state.categoryForm});
   }
 
   render() {
-    console.log('In CategoryItem', this.props);
-    return(
+    return (
       <div className="category-item">
-        <h3 key={this.props.category.id} onDoubleClick={this.toggleUpdate}>{this.props.category.title}: ${this.props.category.budget} budget</h3>
-        <button onClick={() => this.props.categoryDelete(this.props.category)}>delete</button>
-        {this.state.categoryEdit ?
-          <CategoryForm
-            buttonText="update"
-            onComplete={this.props.categoryUpdate}
-            category={this.props.category}/> :
-          undefined
-        }
-        <ExpenseForm 
-          categoryId ={this.props.category.id}
-          buttonText="add"
-          onComplete={this.props.expenseCreate} />
+        <div className="content-container">
+          <button className="remove" onClick={() => this.props.categoryDelete(this.props.category)}>Delete</button>
+          <button onClick={this.toggleExpense}>New</button>
+          <h3 key={this.props.category.id} onDoubleClick={this.toggleCategory}>{this.props.category.title}: ${this.props.category.budget} budget</h3>
+
+          {this.state.categoryForm ?
+            <CategoryForm
+              buttonText="update"
+              onComplete={this.props.categoryUpdate}
+              category={this.props.category}
+              toggle={this.toggleCategory}/> :
+            undefined
+          }
+        </div>
+        <div className="content-container">
+          {this.state.expenseForm ?
+            <ExpenseForm
+              buttonText="create"
+              categoryId={this.props.category.id}
+              onComplete={this.props.expenseCreate}
+              toggle={this.toggleExpense}/> :
+            undefined
+          }
+
+          {this.props.expense[this.props.category.id].length ?
+            this.props.expense[this.props.category.id].map(expense => <ExpenseItem key={expense.id} expense={expense}/>)
+            :
+            <h3>You Currently Have No Expenses</h3>
+          }
+        </div>
       </div>
     );
   }
 }
 
-let mapStateToProps = () => ({});
+let mapStateToProps = state => {
+  return {
+    expense: state.expense,
+  };
+};
 
 let mapDispatchToProps = (dispatch, getState) => {
-  return{
+  return {
     categoryUpdate: category => dispatch(categoryUpdate(category)),
     categoryDelete: category => dispatch(categoryDelete(category)),
     expenseCreate: expense => dispatch(expenseCreate(expense)),
--- lab-isaac/src/reducer/expense.js
+++ lab-isaac/src/reducer/expense.js
@@ -2,24 +2,26 @@ let initialState = {};
 
 export default (state=initialState, action) => {
   let{type, payload} = action;
-
   switch(type) {
   case 'CATEGORY_CREATE': return {...state, [payload.id]: []};
   case 'CATEGORY_DELETE': return {...state, [payload.id]: null};
+  
   case 'EXPENSE_CREATE':
-  categoryId = payload;
-    let categoryExpenses = state[categoryId];
-    return {...state, [categoryId]: [...categoryExpenses, payload]};
+      let categoryExpenses = state[payload.categoryId];
+      return {...state, [payload.categoryId]: [...categoryExpenses, payload]};
+  
   case 'EXPENSE_UPDATE':
-  let updateState = state;
-    categoryId = payload.categoryId;
-    updateState[categoryId] = updateState[categoryId].map(expense => {
-      if(expense.id === payload.id) expense = payload;
-      return expense;
-    });
-    return {...updateState};
+    let updateState = state;
+      updateState[payload.categoryId] = updateState[payload.categoryId].map(expense => {
+        if(expense.id === payload.id) expense = payload;
+        return expense;
+      });
+      return {...updateState}
+  
   case 'EXPENSE_DELETE':
-    return
+      let deleteState = state;
+      deleteState[payload.categoryId] = deleteState[payload.categoryId].filter(expense => expense.id !== payload.id)
+    return {...deleteState};
   default: return state;
   }
 };
